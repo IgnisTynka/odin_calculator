402d0b0918fe370f583768ce01db1e879f955c5a
Tynka <marcia03835@gmail.com>
2022-07-16 12:52:53 +0200

    Modified operator events and improved clear functions

--- script.js
+++ script.js
@@ -4,62 +4,69 @@ let equalDiv = document.getElementById('result')
 const numbers = document.querySelectorAll('.numbers');
 let operators = document.querySelectorAll('.operators')
 
-let currentNumber = '';
-let firstNumber = 0;
-let selectedOperator = '';
+// let currentNumber = '';
+// let firstNumber = 0;
+// let selectedOperator = '';
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
         equalDiv.innerText = '';
         operationDiv.innerText += number.value;
-        currentNumber += number.value;
-        console.log(currentNumber)
+        // currentNumber += number.value;
     })
 });
 
 decimal.addEventListener('click', () => {    
-    if(currentNumber == ''){
-        currentNumber = '0.'
-        operationDiv.innerText = currentNumbert;
+    if(operationDiv.innerText == ''){
+        // currentNumber = '0.'
+        operationDiv.innerText = '0.';
     }else{
-        currentNumber += '.'
+        // currentNumber += '.'
         operationDiv.innerText += '.';
     }
 })
 
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        operationDiv.innerText += operator.innerText;
+        operationDiv.innerText += operator.value;
         selectedOperator = operator.value;
-        firstNumber = currentNumber;
-        currentNumber = '';
+        // firstNumber = currentNumber;
+        // currentNumber = '';
     })
 });
 
 equal.addEventListener('click', () => {
     let result = 0;
+    let numbersArray = []; 
     if(selectedOperator == '+'){
-        result = Number(firstNumber) + Number(currentNumber);
+        numbersArray = operationDiv.innerText.split('+');
+        result = Number(numbersArray[0]) + Number(numbersArray[1]);
         equalDiv.innerText = result;
     }else if(selectedOperator == '-'){
-        result = Number(firstNumber) - Number(currentNumber);
+        numbersArray = operationDiv.innerText.split('-');
+        result = Number(numbersArray[0]) - Number(numbersArray[1]);
         equalDiv.innerText = result;
     }else if(selectedOperator == '*'){
-        result = Number(firstNumber) * Number(currentNumber);
+        numbersArray = operationDiv.innerText.split('*');
+        console.log(numbersArray);
+        result = Number(numbersArray[0]) * Number(numbersArray[1]);
         equalDiv.innerText = result;
     }else if(selectedOperator == '/'){
-        result = Number(firstNumber) / Number(currentNumber);
+        numbersArray = operationDiv.innerText.split('/');
+        console.log(numbersArray);
+        result = Number(numbersArray[0]) / Number(numbersArray[1]);
         equalDiv.innerText = result;
     }
 })
 
 allclear.addEventListener('click', () => {
-    let currentNumber = '';
-    let firstNumber = 0;
-    let selectedOperator = '';
+    // currentNumber = '';
+    // firstNumber = 0;
+    // selectedOperator = '';
     operationDiv.innerText = '';
     equalDiv.innerText = '';
 })
 
 oneclear.addEventListener('click', () => {
+    operationDiv.innerText = operationDiv.innerText.slice(0, -1)  
 })
