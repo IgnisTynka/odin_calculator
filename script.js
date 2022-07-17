4f8e7617832d86cd892a3550500f7fed2ec4d4e2
Tynka <marcia03835@gmail.com>
2022-07-17 13:41:25 +0200

    Modified all events and change of the method of calculating the result

--- script.js
+++ script.js
@@ -1,23 +1,69 @@
 let operationDiv = document.getElementById('calculations');
-let equalDiv = document.getElementById('result')
+let equalDiv = document.getElementById('result');
 
 const numbers = document.querySelectorAll('.numbers');
-let operators = document.querySelectorAll('.operators')
+const operators = document.querySelectorAll('.operators')
+const operatorsSymbol = ['+', '-', '*', '/', '%'];
+
+let selectedOperator = new Array;
+let operatorNumber = 0;
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        equalDiv.innerText = '';
-        operationDiv.innerText += number.value;
-        let firstPart = operationDiv.innerText;
-        operators.forEach(operator => {
-            operator.addEventListener('click', () => {
-                operationDiv.innerText = firstPart + operator.value;
-                selectedOperator = operator.value;
-                decimal.disabled = false;
-            })
-        });
+        if(equalDiv.innerText != ''){
+            equalDiv.innerText = '';
+            operationDiv.innerText = '';
+        }
+        if(operatorsSymbol.includes(operationDiv.innerText.slice(-1))){
+            operatorNumber++;
+        }
+        
+        operationDiv.innerText += number.value; 
     })
 });
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        if(operatorsSymbol.includes(operationDiv.innerText.slice(-1))){
+            operationDiv.innerText = operationDiv.innerText.slice(0, -1) 
+            operationDiv.innerText += operator.value;
+        }else{
+            operationDiv.innerText += operator.value;
+            decimal.disabled = false;
+        }
+        selectedOperator[operatorNumber] = operator.value;
+        console.log(selectedOperator);
+    })
+});
+
+equal.addEventListener('click', () => {
+    let result = 0;
+    let numbersArray = [];
+    let splitOperators = new RegExp()
+    numbersArray = operationDiv.innerText.split(/[{+}\-{*}{/}{%}]/);
+    console.log(numbersArray);
+    for(i = 0; i < operatorNumber; i++){
+        if(selectedOperator[i] == '+'){
+            result = Number(numbersArray[0]) + Number(numbersArray[1]); 
+        }else if(selectedOperator[i] == '-'){
+            result = Number(numbersArray[0]) - Number(numbersArray[1]);
+        }else if(selectedOperator[i] == '*'){
+            result = Number(numbersArray[0]) * Number(numbersArray[1]);
+        }else if(selectedOperator[i] == '/'){
+            result = Number(numbersArray[0]) / Number(numbersArray[1]);
+        }else if(selectedOperator[i] == '%'){
+            result = Number(numbersArray[0]) % Number(numbersArray[1]);
+        }
+        numbersArray.shift();
+        console.log(numbersArray);
+        numbersArray[0] = result;
+
+    }
+    equalDiv.innerText = result;
+    selectedOperator = [];
+    operatorNumber = 0;
+    decimal.disabled = false;
+})
 
 decimal.addEventListener('click', () => {    
     if(operationDiv.innerText == ''){
@@ -28,41 +74,19 @@ decimal.addEventListener('click', () => {
     decimal.disabled = true;
 })
 
-equal.addEventListener('click', () => {
-    let result = 0;
-    let numbersArray = []; 
-    if(selectedOperator == '+'){
-        numbersArray = operationDiv.innerText.split('+');
-        result = Number(numbersArray[0]) + Number(numbersArray[1]); 
-    }else if(selectedOperator == '-'){
-        numbersArray = operationDiv.innerText.split('-');
-        result = Number(numbersArray[0]) - Number(numbersArray[1]);
-    }else if(selectedOperator == '*'){
-        numbersArray = operationDiv.innerText.split('*');
-        result = Number(numbersArray[0]) * Number(numbersArray[1]);
-    }else if(selectedOperator == '/'){
-        numbersArray = operationDiv.innerText.split('/');
-        result = Number(numbersArray[0]) / Number(numbersArray[1]);
-    }
-    equalDiv.innerText = result;
-    operators.forEach(operator => {
-        operator.addEventListener('click', () => {
-            operationDiv.innerText = result + operator.value;
-            selectedOperator = operator.value;
-        })
-    });
-    decimal.disabled = false;
-})
-
 allclear.addEventListener('click', () => {
     operationDiv.innerText = '';
     equalDiv.innerText = '';
+    selectedOperator = [];
+    operatorNumber = 0;
     decimal.disabled = false;
 })
 
 oneclear.addEventListener('click', () => {
     if(operationDiv.innerText.slice(-1) == '.'){
         decimal.disabled = false;
-    } 
+    }else if(operatorsSymbol.includes(operationDiv.innerText.slice(-1))){
+        selectedOperator.pop();
+    }
     operationDiv.innerText = operationDiv.innerText.slice(0, -1) 
 })
